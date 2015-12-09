44e868c1e72d226a7dbc5fe3580ff72ca0497cc0
jonschwadron <jonschwadron@gmail.com>
2015-12-08 23:45:54 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+angular.module('Instagram', ['ngRoute', 'ngMessages', 'satellizer'])
+  .config(function($routeProvider, $authProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
+      .when('/signup', {
+        templateUrl: 'views/signup.html',
+        controller: 'SignupCtrl'
+      })
+      .when('/photo/:id', {
+        templateUrl: 'views/detail.html',
+        controller: 'DetailCtrl'
+      })
+      .otherwise('/');
+
+    $authProvider.loginUrl = 'http://snowpro.herokuapp.com/auth/login';
+    $authProvider.signupUrl = 'http://snowpro.herokuapp.com/auth/signup';
+    $authProvider.oauth2({
+      name: 'snowpro',
+      url: 'https://snowpro.herokuapp.com/auth/instagram',
+      redirectUri: 'https://dl.dropboxusercontent.com/u/57700357/index.html',
+      clientId: 'a3d34fd164894c35aaed50d4ff55e3ca',
+      requiredUrlParams: ['scope'],
+      scope: ['likes'],
+      scopeDelimiter: '+',
+      authorizationEndpoint: 'https://api.instagram.com/oauth/authorize'
+    });
+  })
+  .run(function($rootScope, $window, $auth) {
+    if ($auth.isAuthenticated()) {
+      $rootScope.currentUser = JSON.parse($window.localStorage.currentUser);
+    }
+  });
