6a54d563a0314f05ec48700d5b45b2911695b601
jonschwadron <jonschwadron@gmail.com>
2015-12-09 01:11:23 -0500

    update

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ angular.module('Instagram', ['ngRoute', 'ngMessages', 'satellizer'])
     $authProvider.signupUrl = 'https://snowpro.herokuapp.com/auth/signup';
     $authProvider.oauth2({
       name: 'snowpro',
-      url: 'https://snowpro.herokuapp.com/auth/instagram',
-      redirectUri: '	http://jonschwadron.github.io/snowpro_client',
+      url: '/auth/instagram',
+      redirectUri: 'https://jonschwadron.github.io/snowpro_client',
       clientId: 'a3d34fd164894c35aaed50d4ff55e3ca',
       requiredUrlParams: ['scope'],
       scope: ['likes'],
