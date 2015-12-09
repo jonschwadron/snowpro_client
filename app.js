cd18955c6da9c08e4d19f15ada3b3149bfe266b1
jonschwadron <jonschwadron@gmail.com>
2015-12-09 10:34:51 -0500

    update

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ angular.module('Instagram', ['ngRoute', 'ngMessages', 'satellizer'])
       })
       .otherwise('/');
 
-    $authProvider.loginUrl = 'http://snowpro.herokuapp.com/auth/login';
-    $authProvider.signupUrl = 'http://snowpro.herokuapp.com/auth/signup';
+    $authProvider.loginUrl = 'https://snowpro.herokuapp.com/auth/login';
+    $authProvider.signupUrl = 'https://snowpro.herokuapp.com/auth/signup';
     $authProvider.oauth2({
       name: 'instagram',
-      url: 'http://snowpro.herokuapp.com/auth/instagram',
-      redirectUri: 'http://jonschwadron.github.io/snowpro_client/index.html',
+      url: 'https://snowpro.herokuapp.com/auth/instagram',
+      redirectUri: 'https://jonschwadron.github.io/snowpro_client/index.html',
       clientId: 'a3d34fd164894c35aaed50d4ff55e3ca',
       requiredUrlParams: ['scope'],
       scope: ['likes'],
--- services/api.js
+++ services/api.js
@@ -3,13 +3,13 @@ angular.module('Instagram')
 
       return {
         getFeed: function() {
-          return $http.get('//snowpro.herokuapp.com/api/feed');
+          return $http.get('https://snowpro.herokuapp.com/api/feed');
         },
         getMediaById: function(id) {
-          return $http.get('//snowpro.herokuapp.com/api/media/' + id);
+          return $http.get('https://snowpro.herokuapp.com/api/media/' + id);
         },
         likeMedia: function(id) {
-          return $http.post('//snowpro.herokuapp.com/api/like', { mediaId: id });
+          return $http.post('https://snowpro.herokuapp.com/api/like', { mediaId: id });
         }
       }
 
