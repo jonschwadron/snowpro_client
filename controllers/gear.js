13875431d70340e962dd58098b3ac3d3dbec6d8f
jonschwadron <jonschwadron@gmail.com>
2015-12-10 20:51:51 -0500

    added gear page

---
+++ controllers/gear.js
@@ -0,0 +1,43 @@
+angular.module('MyApp')
+  .controller('GearCtrl', function($scope, $auth, toastr, Account) {
+    $scope.getGear = function() {
+      Account.getGear()
+        .then(function(response) {
+          $scope.user = response.data;
+        })
+        .catch(function(response) {
+          toastr.error(response.data.message, response.status);
+        });
+    };
+    $scope.updateGear = function() {
+      Account.updateGear($scope.user)
+        .then(function() {
+          toastr.success('Gear has been updated');
+        })
+        .catch(function(response) {
+          toastr.error(response.data.message, response.status);
+        });
+    };
+    $scope.link = function(provider) {
+      $auth.link(provider)
+        .then(function() {
+          toastr.success('You have successfully linked a ' + provider + ' account');
+          $scope.getGear();
+        })
+        .catch(function(response) {
+          toastr.error(response.data.message, response.status);
+        });
+    };
+    $scope.unlink = function(provider) {
+      $auth.unlink(provider)
+        .then(function() {
+          toastr.info('You have unlinked a ' + provider + ' account');
+          $scope.getGear();
+        })
+        .catch(function(response) {
+          toastr.error(response.data ? response.data.message : 'Could not unlink ' + provider + ' account', response.status);
+        });
+    };
+
+    $scope.getGear();
+  });
