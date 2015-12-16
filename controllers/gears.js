caf2056673dedee383fef8c71f8777957f425244
jonschwadron <jonschwadron@gmail.com>
2015-12-15 23:36:31 -0500

    more modal tests

--- controllers/gears.js
+++ controllers/gears.js
@@ -1,5 +1,9 @@
 angular.module('MyApp')
   .controller('GearsCtrl', function($scope, $auth, toastr, Account) {
+    $scope.showModal = false;
+    $scope.toggleModal = function(){
+        $scope.showModal = !$scope.showModal;
+    };
     // $scope.getGears = function() {
     //   Account.getGears()
     //     .then(function(response) {
@@ -76,59 +80,4 @@ angular.module('MyApp')
     $scope.getSnowboard();
     $scope.getBindings();
     $scope.getBoots();
-  });
-
-angular.module('MyApp', ['ngAnimate', 'ui.bootstrap']);
-angular.module('MyApp')
-  .controller('ModalDemoCtrl', function ($scope, $uibModal, $log) {
-
-    $scope.items = ['item1', 'item2', 'item3'];
-
-    $scope.animationsEnabled = true;
-
-    $scope.open = function (size) {
-
-      var modalInstance = $uibModal.open({
-        animation: $scope.animationsEnabled,
-        templateUrl: 'myModalContent.html',
-        controller: 'ModalInstanceCtrl',
-        size: size,
-        resolve: {
-          items: function () {
-            return $scope.items;
-          }
-        }
-      });
-
-      modalInstance.result.then(function (selectedItem) {
-        $scope.selected = selectedItem;
-      }, function () {
-        $log.info('Modal dismissed at: ' + new Date());
-      });
-    };
-
-    $scope.toggleAnimation = function () {
-      $scope.animationsEnabled = !$scope.animationsEnabled;
-    };
-
-  });
-
-// Please note that $modalInstance represents a modal window (instance) dependency.
-// It is not the same as the $uibModal service used above.
-
-angular.module('MyApp')
-  .controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
-
-    $scope.items = items;
-    $scope.selected = {
-      item: $scope.items[0]
-    };
-
-    $scope.ok = function () {
-      $uibModalInstance.close($scope.selected.item);
-    };
-
-    $scope.cancel = function () {
-      $uibModalInstance.dismiss('cancel');
-    };
   });
