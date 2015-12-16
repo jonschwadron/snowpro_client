26623c4b0ca86d2dd34786113b813521def0a6f8
jonschwadron <jonschwadron@gmail.com>
2015-12-16 05:28:32 -0500

    adjustments

--- controllers/map.js
+++ controllers/map.js
@@ -1,4 +1,4 @@
 angular.module('MyApp')
   .controller('MapCtrl', function($scope, $sce) {
-    $scope.snowboard_emoji = $sce.trustAsResourceUrl('images/snowboard_emoji.png');
+    $scope.snowboard_emoji = $sce.trustAsResourceUrl('../images/snowboard_emoji.png');
   });
