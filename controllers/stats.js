69bb73752e7fa797729d7b4daf44b3fccf3e6ec4
jonschwadron <jonschwadron@gmail.com>
2015-12-18 04:47:12 -0500

    you can haz table

--- controllers/stats.js
+++ controllers/stats.js
@@ -1,4 +1,5 @@
 angular.module('MyApp')
   .controller('StatsCtrl', function($scope) {
-    $scope.snowboard_emoji = "http://jonschwadron.github.io/snowpro_client/images/snowboard_emoji.png";
+    // $scope.snowboard_emoji = "http://jonschwadron.github.io/snowpro_client/images/snowboard_emoji.png";
+    
   });
