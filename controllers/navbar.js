68db7e60e96fbed5d26a436f388cdd52e95baab2
jonschwadron <jonschwadron@gmail.com>
2015-12-18 23:39:37 -0500

    navbar stuff

--- controllers/navbar.js
+++ controllers/navbar.js
@@ -1,6 +1,15 @@
 angular.module('MyApp')
-  .controller('NavbarCtrl', function($scope, $auth) {
+  .controller('NavbarCtrl', function($scope, $auth, toastr, Account) {
     $scope.isAuthenticated = function() {
       return $auth.isAuthenticated();
+    };
+    $scope.getProfile = function() {
+      Account.getProfile()
+        .then(function(response) {
+          $scope.user = response.data;
+        })
+        .catch(function(response) {
+          toastr.error(response.data.message, response.status);
+        });
     };
   });
