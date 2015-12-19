e42bf940f2fc98f27322bbce2859d0dedb8b6baa
jonschwadron <jonschwadron@gmail.com>
2015-12-19 18:50:23 -0500

    changed displayName to profile for now

--- controllers/navbar.js
+++ controllers/navbar.js
@@ -1,16 +1,6 @@
 angular.module('MyApp')
-  .controller('NavbarCtrl', function($scope, $auth, $http, toastr, Account) {
+  .controller('NavbarCtrl', function($scope, $auth) {
     $scope.isAuthenticated = function() {
       return $auth.isAuthenticated();
     };
-    $scope.getDisplayName = function() {
-      Account.getDisplayName()
-        .then(function(response) {
-          $scope.user = response.data;
-        })
-        .catch(function(response) {
-          toastr.error(response.data.message, response.status);
-        });
-    };
-    $scope.getDisplayName();
   });
