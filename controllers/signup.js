960b81adc39b459d70c1d6fdfb2d37140ef4dca1
jonschwadron <jonschwadron@gmail.com>
2015-12-19 21:15:56 -0500

    debug

--- controllers/signup.js
+++ controllers/signup.js
@@ -1,5 +1,5 @@
 angular.module('MyApp')
-  .controller('SignupCtrl', function($scope, $auth, $location, toastr) {
+  .controller('SignupCtrl', function($scope, $location, $auth, toastr) {
     $scope.signup = function() {
       $auth.signup($scope.user)
         .then(function(response) {
