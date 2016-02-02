e6bb1083917ebd1eaa431faffe1d0fabcd4d0b4f
jonschwadron <jonschwadron@gmail.com>
2016-02-02 11:05:20 -0500

    update

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ angular.module('MyApp', ['ngResource', 'ngMessages', 'ngAnimate', 'toastr', 'ui.
       .state('home', {
         url: '/',
         controller: 'HomeCtrl',
-        templateUrl: 'partials/home.html',
-        resolve: {
-          loginRequired: loginRequired
-        }
+        templateUrl: 'partials/news.html',
+        // resolve: {
+        //   loginRequired: loginRequired
+        // }
       })
       .state('login', {
         url: '/login',
